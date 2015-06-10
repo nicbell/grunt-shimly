c479ea8a76f4095546be5cdf0d68061d8502b32d
Nic Bell <nic.bell@hogarthww.com>
2015-06-10 18:34:16 +0100

    Separated shimly core front grunt plugin

--- tasks/shimly.js
+++ tasks/shimly.js
@@ -1,6 +1,6 @@
 /*
  * grunt-shimly
- * https://github.com/nicbell/Shimly
+ * https://github.com/nicbell/grunt-shimly
  */
 
 'use strict';
@@ -8,44 +8,15 @@
 module.exports = function (grunt) {
 
     //load requires
-    var uglify = require('uglify-js'),
-        fs = require('fs'),
-        path = require('path');
-
+    var shimly = require('shimly');
 
     //Tasks
     grunt.registerTask('shimly', 'Builds a set of shims', function () {
         createShims(grunt.config.get('shimly'));
     });
 
-
     //Create shims
     var createShims = function (config) {
-        var files = config.shim,
-            banner = '/*! \nIncluded shims: ' + files + '\n*/\n\n';
-
-        grunt.log.writeln(files);
-
-        //Convert files into paths
-        for (var i = 0; i < files.length; i++) {
-            files[i] = path.normalize(__dirname + '/../src/' + files[i] + '.js');
-        }
-
-        var uglified;
-        var dest = path.resolve(config.dest);
-        var destDir = path.dirname(dest);
-
-        if (config.minify) {
-            uglified = uglify.minify(files, { output: null, preserveComments: false, compress: {} });
-        } else {
-            uglified = uglify.minify(files, { output: { comments: true, beautify: true }, compress: null });
-        }
-
-        if (!fs.existsSync(destDir)) {
-            fs.mkdirSync(destDir);
-        }
-        fs.writeFileSync(dest, banner + uglified.code);
-
-        grunt.log.writeln('Shims created at: ' + dest);
+        shimly(config.shim, config.minify, config.dest);
     };
 };
